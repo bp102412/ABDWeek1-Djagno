93080e9937f6665de61ecbb50dfdd7adc300ff52
Brenden Price <price.brenden01@gmail.com>
2021-04-19 04:53:01 -0400

    Added a test view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Blog
+
 
 # Create your views here.
+def blog(request):
+    blog_posts = Blog.objects.all()
+    context = {'posts': blog_posts, }
+    return render(request, "blogTemplates/blog.html", context)
